9f26212898954b8d15a25c17db21e2557f8ea67c
Luqman Herifa <luqmanherifa@gmail.com>
2026-02-13 17:31:37 +0700

    Update app style

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UsersPage from "./pages/UsersPage";
 import LoginPage from "./pages/LoginPage";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<UsersPage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<UsersPage />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
