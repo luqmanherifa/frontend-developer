0c8cfd425ddd4aaa39984c18d7a0c27e0a837e0d
Luqman Herifa <luqmanherifa@gmail.com>
2026-02-13 17:06:08 +0700

    Add navbar and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UsersPage from "./pages/UsersPage";
+import LoginPage from "./pages/LoginPage";
+import Navbar from "./components/Navbar";
 
 export default function App() {
-  return <UsersPage />;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<UsersPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
